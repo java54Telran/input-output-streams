6a840eb09d41db890eac2758e50e742fdb0a123d
Yuragranovsky <yuragranovsky@gmail.com>
2024-07-11 18:15:23 +0300

    HW #35 solution

--- codeFile.java
+++ codeFile.java
@@ -24,10 +24,7 @@ class InputOutputTest {
 	Files.deleteIfExists(Path.of(WRITER_FILE));
 }
 	@Test
-	    //Testing PrintStream class
-	//creates PrintStream object and connects it to file with name STREAM_FILE
 	void printStreamTest() throws Exception {
-		//try resources block allows automatic closing after existing from the block
 		try(PrintStream printStream = new PrintStream(STREAM_FILE);) {
 			printStream.println(HELLO);
 		}
@@ -38,10 +35,6 @@ class InputOutputTest {
 		}
 	}
 	@Test
-	     //The same test as for PrintStream but for PrintWriter
-	//The difference between PrintSTream and PrintWriter is that
-	// PrintStream puts line immediately while PrintWriter puts line into a buffer
-	// The buffer flushing is happening after closing the stream
 	void printWriterTest() throws Exception {
 		try (PrintWriter printWriter = new PrintWriter(WRITER_FILE)) {
 			printWriter.println(HELLO);
@@ -54,8 +47,6 @@ class InputOutputTest {
 	@Test
 	void pathTest() {
 		Path pathCurrent = Path.of(".");
-//		System.out.printf("%s - path \".\"\n",pathCurrent);
-//		System.out.printf("%s - normolized path \".\"\n", pathCurrent.normalize());
 		System.out.printf("%s - %s\n",
 				pathCurrent.toAbsolutePath().normalize(),
 				Files.isDirectory(pathCurrent) ? "directory" : "file");
@@ -69,11 +60,6 @@ class InputOutputTest {
 		printDirectory("/", 2);
 	}
 	private void printDirectory(String dirPathStr, int depth) throws IOException {
-		//print directory content in the format with offset according to the level
-		//if depth == -1 all levels should be printed out
-		// <name> - <dir / file>
-		//      <name> 
-		//using FIles.walkFileTree
 		Path pathParam = Path.of(dirPathStr);
 		if (!Files.isDirectory(pathParam)) {
 			throw new IllegalArgumentException("not directory");
--- src/telran/io/CodeCommentsSeparation.java
+++ src/telran/io/CodeCommentsSeparation.java
@@ -1,5 +1,9 @@
 package telran.io;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.*;
+record SourceCodeComments(Path source, String code, String comments) {}
 public class CodeCommentsSeparation {
 
 	public static void main(String[] args) {
@@ -7,9 +11,43 @@ public class CodeCommentsSeparation {
 		//args[1] - result file with only code
 		//args[2] -result file with only comments
 		// example of args[0] "src/telran/io/test/InputOutputTest.java" 
-		//TODO 
 		//from one file containing code and comments to create two files
 		//one with only comments and second with only code
+		try {
+			SourceCodeComments scc = processArguments (args);
+			codeCommentsSeparation(scc);
+		} catch (RuntimeException e) {
+			
+			e.printStackTrace();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+	private static void codeCommentsSeparation(SourceCodeComments scc) throws Exception {
+		try(BufferedReader reader = Files.newBufferedReader(scc.source());
+				PrintWriter commentsWriter = new PrintWriter(scc.comments());
+				PrintWriter codeWriter = new PrintWriter(scc.code())) {
+			reader.lines().forEach(l -> (l.trim().startsWith("//") ?
+					commentsWriter : codeWriter).println(l));
+		}
+		
+	}
+
+	private static SourceCodeComments processArguments (String[] args)throws Exception {
+		if(args.length != 3) {
+			throw new Exception("too few arguments");
+		}
+		Path sourcePath = Path.of(args[0]);
+		
+		if (!Files.exists(sourcePath)) {
+			throw new Exception(String.format("%s doesn't exist",
+					sourcePath.toAbsolutePath().normalize()));
+		}
+		
+		return new SourceCodeComments(sourcePath, args[1], args[2]);
 	}
 
 }
